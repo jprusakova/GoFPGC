8b963a5a580f3ffcc54a96e209837075e915b2c7
jprusakova <jane.prusakova@improving>
2017-06-27 12:27:06 -0500

    Test update for Prototype pattern

--- Prototype/PrototypeTest/PrototypeCloneTest.cs
+++ Prototype/PrototypeTest/PrototypeCloneTest.cs
@@ -6,27 +6,29 @@ namespace PrototypeTest
     [TestClass]
     public class PrototypeCloneTest
     {
-        Dinner _dinner;
+        Dinner _steakDinner;
+        Dinner _vegetarianDinner; 
         Dinner _clonedDinner;
+        Dinner _clonedVegetarianOption;
 
         [TestInitialize]
         public void Setup()
         {
-            _dinner = new Dinner("Main Course", "Dessert")
-            {
-                Restaurant = "Restaurant",
-                ChefName = "Chef"
-            };
-            _clonedDinner = (Dinner)_dinner.Clone();
-            _clonedDinner.ChefName = "Another Chef";
-            _clonedDinner.Restaurant = "Better Restaurant";
+            SetupSteakDinner();
+            SetupVegetarianDinner();
         }
 
         [TestMethod]
         public void VerifyClonedProperties()
         {
-            Assert.AreEqual(_dinner.MainCourse, _clonedDinner.MainCourse);
-            Assert.AreEqual(_dinner.Dessert, _clonedDinner.Dessert);
+            Assert.AreEqual(_steakDinner.MainCourse, _clonedDinner.MainCourse);
+            Assert.AreEqual(_steakDinner.Dessert, _clonedDinner.Dessert);
+
+            Assert.AreEqual(_vegetarianDinner.MainCourse, _clonedVegetarianOption.MainCourse);
+            Assert.AreEqual(_vegetarianDinner.Dessert, _clonedVegetarianOption.Dessert);
+
+            Assert.AreNotEqual(_clonedDinner.MainCourse, _clonedVegetarianOption.MainCourse);
+            Assert.AreNotEqual(_clonedDinner.Dessert, _clonedVegetarianOption.Dessert);
         }
 
         [TestMethod]
@@ -39,8 +41,32 @@ namespace PrototypeTest
         [TestMethod]
         public void OriginalAdditionalPropertiesTest()
         {
-            Assert.AreNotEqual(_dinner.ChefName, _clonedDinner.ChefName);
-            Assert.AreNotEqual(_dinner.Restaurant, _clonedDinner.Restaurant);
+            Assert.AreNotEqual(_steakDinner.ChefName, _clonedDinner.ChefName);
+            Assert.AreNotEqual(_steakDinner.Restaurant, _clonedDinner.Restaurant);
+        }
+
+        private void SetupVegetarianDinner()
+        {
+            _vegetarianDinner = new Dinner("Beans and Vegetables", "Cake")
+            {
+                Restaurant = "Vegetarian Restaurant",
+                ChefName = "TV Personality"
+            };
+
+            _clonedVegetarianOption = (Dinner)_vegetarianDinner.Clone();
+        }
+
+        private void SetupSteakDinner()
+        {
+            _steakDinner = new Dinner("Steak And Potatoes", "Dessert")
+            {
+                Restaurant = "Restaurant",
+                ChefName = "Meat Butcher"
+            };
+
+            _clonedDinner = (Dinner)_steakDinner.Clone();
+            _clonedDinner.ChefName = "Another Chef";
+            _clonedDinner.Restaurant = "Better Restaurant";
         }
     }
 }
